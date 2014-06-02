201a86c568e48d690082d9497707798b2ce596a0
intlabs <petebirley@gmail.com>
2014-06-02 01:20:40 +0100

    initial

---
+++ spawn-desktop.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# (c) Pete Birley
+
+#this sets the vnc password
+/usr/local/etc/start-vnc-expect-script.sh
+
+#fixes a warning with starting nautilus on firstboot - which we will always be doing.
+mkdir -p ~/.config/nautilus
+
+#this starts the vnc server
+USER=root vncserver :1 -geometry 1366x768 -depth 24
+
+#this starts noVNC
+/noVNC/utils/launch.sh --vnc 127.0.0.1:5901 --listen 80
